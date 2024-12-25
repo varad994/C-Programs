c28db1d9f30af3ebb0c350c0c5c0c1e807d9688b
Varad Pawar <varadapawar0904@gmail.com>
2024-12-25 11:12:54 +0530

    Add files via upload

---
+++ Factorial.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main() {
+    int n, i, factorial = 1;
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; ++i) {
+        factorial *= i;
+    }
+    printf("Factorial of %d = %d\n", n, factorial);
+
+    return 0;
+}
